d4dc14eee558195f58cadf6d2fd80917cfd22b2e
Simon Alfassa <simon@sa-web.fr>
2019-08-07 00:33:19 +0200

    Add more interesting stats
    - most common typos
    - most failed example

--- gotypist_stats.py
+++ gotypist_stats.py
@@ -38,6 +38,16 @@ def _to_datetime(iso):
     return datetime.strptime(parsable, "%Y-%m-%dT%H:%M:%S.%f|%z")
 
 
+def _human_duration(start, end):
+    secs = int((end - start).total_seconds())
+    if secs <= 120:
+        return f"{secs} seconds"
+    elif secs <= 3600 * 2:
+        return f"~{secs // 60} minutes"
+    else:
+        return f"~{secs // 3600} hours"
+
+
 def read_stats(stats_file):
     with open(stats_file) as f:
         for line in f:
@@ -72,6 +82,7 @@ def hitmap(day, stats):
 
 
 def render_hitmap(hitmap):
+    print("🟄 6 months hitmap 🟄")
     (y1, w1, _) = hitmap["start"].isocalendar()
     coords = lambda day, week: (y1 + (w1 + week) // 52, (w1 + week) % 52, day + 1)
     char = lambda treshold, v: "▓▓" if v >= treshold else "▒▒" if v > 0 else "░░"
@@ -84,6 +95,42 @@ def render_hitmap(hitmap):
         line = "".join(map(lambda p: char(hitmap["median"], p), practice))
         print(f"{day_abbr[day]} {line}")
 
+
+def typo_record(day, stats):
+    return sorted(stats, key=lambda s: s.errors, reverse=True)[0]
+
+
+def render_typo_record(worse):
+    print("\n🟄 Highest number of typos 🟄")
+    print(
+        f"""{worse.errors} errors\
+ when typing ʺ{worse.text}ʺ\
+ in {worse.mode.name.lower()} mode\
+ on {worse.started_at.strftime("%b %m %Y")}. It took {_human_duration(worse.started_at, worse.finished_at)}"""
+    )
+
+
+def common_typos(day, stats):
+    typos = defaultdict(int)
+    total = 0
+    for stat in stats:
+        if stat.errors > 0:
+            for typo in stat.typos:
+                typos[typo] += 1
+            total += len(stat.typos)
+
+    return (sorted(typos.items(), key=lambda i: i[1], reverse=True)[:6], total)
+
+
+def render_common_typos(typos, total):
+    print("\n🟄 Most common errors 🟄")
+    for idx, (spec, count) in enumerate(typos):
+        explanation = idx == 0 and " of all mistakes" or ""
+        print(
+            f"{spec.actual} instead of {spec.expected}: {count} errors ({100*count/total:.1f}%{explanation})"
+        )
+
+
 if __name__ == "__main__":
     stats_file = (
         path.join(getenv("HOME"), ".gotypist.stats")
@@ -91,6 +138,8 @@ if __name__ == "__main__":
         else sys.argv[1]
     )
     today = date.today()
-    stats = read_stats(stats_file)
+    stats = list(read_stats(stats_file))
 
-    render_hitmap(year_hitmap(today, stats))
+    render_hitmap(hitmap(today, stats))
+    render_typo_record(typo_record(today, stats))
+    render_common_typos(*common_typos(today, stats))
