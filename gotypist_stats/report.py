c57964e1f6a91ceabd1d0ba247a6ba81ce77bbb5
Simon Alfassa <simon@sa-web.fr>
2019-10-24 20:44:25 +0200

    Code cleanup
    - Move report to its own module
    - Add classifer
    - Ignore eggs from git
    - Use less fancy but more compact grid

---
+++ gotypist_stats/report.py
@@ -0,0 +1,135 @@
+from datetime import date, datetime, timedelta
+from calendar import day_abbr
+from collections import defaultdict
+from enum import Enum
+from functools import reduce
+from typing import NamedTuple, List, Sequence as Seq, Dict
+from statistics import median
+
+from tabulate import tabulate
+
+Mode = Enum("Mode", "FAST SLOW NORMAL")
+
+
+class Typo(NamedTuple):
+    expected: str
+    actual: str
+
+
+class Stat(NamedTuple):
+    text: str
+    started_at: datetime
+    finished_at: datetime
+    errors: int
+    typos: List[Typo]
+    mode: Mode
+    seconds: float
+    cps: float
+    wpm: float
+    version: int
+
+
+class Report(NamedTuple):
+    title: str
+    content: str
+
+
+def _human_duration(delta: timedelta) -> str:
+    secs = delta.total_seconds()
+    if secs <= 120:
+        return f"{secs} seconds"
+    elif secs <= 3600 * 2:
+        return f"{secs // 60:.0f} minutes {secs % 60:0.0f}s"
+    else:
+        return f"{secs // 3600:.0f} hours {(secs % 3600)//60:0.0f}min"
+
+
+def hitmap(today: date, stats: Seq[Stat]) -> Report:
+    begin = today - timedelta(days=182)
+    first_monday = begin - timedelta(begin.weekday())
+    last_monday = today - timedelta(today.weekday())
+    nb_weeks = 1 + (last_monday - first_monday).days // 7
+
+    hitmap: Dict[date, int] = defaultdict(int)
+
+    for stat in stats:
+        if stat.started_at.date() < begin:
+            continue
+        hitmap[stat.started_at.date()] += 1
+
+    med = median(hitmap.values())
+    session_count = lambda week, day: hitmap[
+        first_monday + timedelta(days=7 * week + day)
+    ]
+    char = lambda treshold, v: "▓▓" if v >= treshold else "▒▒" if v > 0 else "░░"
+
+    lines = []
+    for weekday in range(7):
+        line = "".join(
+            char(med, session_count(week, weekday)) for week in range(nb_weeks)
+        )
+        lines.append(f"{day_abbr[weekday]} {line}")
+
+    return Report(title="6 months hitmap", content="\n".join(lines))
+
+
+def training_time(stats: Seq[Stat]) -> Report:
+    duration = reduce(
+        lambda total, diff: total + diff,
+        [s.finished_at - s.started_at for s in stats],
+        timedelta(),
+    )
+
+    return Report(
+        title="Overall stats",
+        content=tabulate(
+            [("Total training time:", _human_duration(duration))], tablefmt="grid"
+        ),
+    )
+
+
+def typo_record(stats: Seq[Stat]) -> Report:
+    worse = sorted(stats, key=lambda s: s.errors, reverse=True)[0]
+    return Report(
+        title="Biggest failure",
+        content=tabulate(
+            (
+                ("was typing", worse.text),
+                ("mode", worse.mode.name.lower()),
+                ("failed", f"{worse.errors} times"),
+                ("happened on", worse.started_at.strftime("%b %m %Y")),
+                (
+                    "struggled for",
+                    _human_duration(worse.finished_at - worse.started_at),
+                ),
+            ),
+            tablefmt="grid",
+        ),
+    )
+
+
+def common_typos(stats: Seq[Stat]) -> Report:
+    typos: Dict[Typo, int] = defaultdict(int)
+    total = 0
+    for stat in stats:
+        if stat.errors > 0:
+            for typo in stat.typos:
+                typos[typo] += 1
+            total += len(stat.typos)
+
+    sorted_typos = sorted(typos.items(), key=lambda i: i[1], reverse=True)
+
+    values = [
+        (f"{spec.actual} instead of {spec.expected}", count, f"{count / total:.2%}")
+        for (spec, count) in sorted_typos[:6]
+    ]
+
+    return Report(
+        title="Most common typos",
+        content=tabulate(  # type: ignore
+            values,
+            headers=("Typo", "Mistakes", "% of mistakes"),
+            tablefmt="simple",
+            showindex=range(1, len(values) + 1),  # default value confuses type checker
+        ),
+    )
--- setup.py
+++ setup.py
@@ -17,4 +17,14 @@ setup(
     long_description_content_type="text/markdown",
     keywords="gotypist typing statistics",
     url="https://github.com/ssimono/gotypist-stats",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Natural Language :: English",
+        "Topic :: Education",
+    ],
 )
